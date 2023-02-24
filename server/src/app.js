8127eaaae6a9a15ca72085bfdde60ebec5d483d9
Aleksander Rudolf <aleks.rudolf@outlook.com>
2023-02-24 14:16:58 -0700

    WIP: BE changed src/index.js to src/app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const Database = require("better-sqlite3");
-const db = new Database("jobmatch.db", { verbose: console.log });
 
 const {
 	LoginService,
@@ -23,7 +21,6 @@ app.use("/job-posts", JobPostsService);
 app.use("/apply", ApplyService);
 app.use("/offers", OffersService);
 app.use("/profile", ProfileService);
-
 
 app.listen(3000, () => {
 	console.log("Server is running on port 3000");
