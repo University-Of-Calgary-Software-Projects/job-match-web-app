5ae51f4893280b3737c702602d13b056500c7341
Nour Amer <84950398+NoureldinAmer@users.noreply.github.com>
2023-02-23 19:21:28 -0700

    remove error component from offers page

--- client/src/Pages/Offers.js
+++ client/src/Pages/Offers.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import EmployerOffers from './Employer/Offers'
 import EmployeeOffers from './Employee/Offers'
-import ErrorPage from './ErrorPage'
 
 
 function Offers() {
