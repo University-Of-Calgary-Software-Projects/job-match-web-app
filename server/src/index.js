6a77e7b296b0b9d6d845a33ade115f7c26e23c3e
Nour Amer <84950398+NoureldinAmer@users.noreply.github.com>
2023-03-22 00:49:43 -0600

    implement upload resume endpoint

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const multer = require('multer');
 
 const {
 	LoginService,
@@ -26,6 +27,16 @@ app.use("/profile", ProfileService);
 app.use("/search", SearchService);
 
 
+const upload = multer({ storage: multer.memoryStorage() });
+
 app.listen(3000, () => {
 	console.log("Server is running on port 3000");
 });
+
+app.post('/upload', upload.single('pdf'), (req, res) => {
+	const file = req.file;
+	console.log(file);
+  if (!file) {
+    return res.status(400).send('No file uploaded');
+  }
+})
