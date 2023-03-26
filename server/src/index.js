0f14c096d8ffdc62da7efa2a9f0022c2140d4074
Aleksander Rudolf <aleks.rudolf@outlook.com>
2023-03-26 15:42:38 -0600

    Hot fix

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const multer = require('multer');
+const multer = require("multer");
 
 const {
 	LoginService,
@@ -10,7 +10,6 @@ const {
 	OffersService,
 	ProfileService,
 	SearchService,
-	
 } = require("./services");
 
 const app = express();
@@ -26,19 +25,10 @@ app.use("/offers", OffersService);
 app.use("/profile", ProfileService);
 app.use("/search", SearchService);
 
-
 const upload = multer({ storage: multer.memoryStorage() });
 
 // app.listen(3000, () => {
 // 	console.log("Server is running on port 3000");
 // });
-
-app.post('/upload', upload.single('pdf'), (req, res) => {
-	const file = req.file;
-	console.log(file);
-  if (!file) {
-    return res.status(400).send('No file uploaded');
-  }
-})
 
 module.exports = app;
